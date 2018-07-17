86520f770880e849612fba7bfabfbaaf4a4ae18e
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2018-07-17 14:31:23 +0200

    Update and rename rho_DCCA.jl to DCCA.jl

---
+++ src/DCCA.jl
@@ -0,0 +1,23 @@
+module DCCA
+
+using PyPlot
+include("calculs.jl")
+
+function rho_DCCA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int,plot::Bool)
+    x =  log_space(box_b,box_s,nb_pt)
+    y = rho(data1,data2,box_b,box_s,nb_pt; fit_type = "polynomial")
+    if plot == false
+        return y
+    elseif plot == true
+        plot(x,y,"bo-",markersize = 4); 
+        title("DCCA coefficients analysis of data)
+        legend()
+        xlabel("Window size s (notes)")
+        xscale("log")
+        ylabel(L"$\rho_{DCCA}(s)$")
+    end
+end
+
+export rho_DCCA
+
+end
