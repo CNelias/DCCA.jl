7ad391219018f945422294bfa13583626e36bd10
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2018-07-17 14:44:25 +0200

    Update DCCA.jl

--- src/DCCA.jl
+++ src/DCCA.jl
@@ -9,15 +9,14 @@ function rhoDCCA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int,plot::Bool)
     if plot == false
         return y
     elseif plot == true
-        plot(x,y,"bo-",markersize = 4); 
-        title("DCCA coefficients analysis of data)
+        plot(x,y,"bo-",markersize = 4)
+        title("DCCA coefficients analysis of data")
         legend()
         xlabel("Window size s (notes)")
         xscale("log")
         ylabel(L"$\rho_{DCCA}(s)$")
     end
 end
-
 export rhoDCCA
-
 end
+  
