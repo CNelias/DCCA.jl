67616a2f06bfd8e3e9a34e5e7065088871fb97e3
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2018-07-19 10:43:29 +0200

    Update DCCA.jl

--- src/DCCA.jl
+++ src/DCCA.jl
@@ -3,20 +3,13 @@ module DCCA
 using PyPlot
 include("calculs.jl")
 
-function rhoDCCA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int,plot::Bool)
+function rhoDCCA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int)
     x = log_space(box_b,box_s,nb_pt)
     y = dcca(data1,data2,box_b,box_s,nb_pt; fit_type = "polynomial")
-    if plot == false
-        return y
-    elseif plot == true
-        plot(x,y,"bo-",markersize = 4, label = "strengh of correlation")
-        title("DCCA coefficients analysis of data")
-        legend()
-        xlabel("Window size s")
-        xscale("log")
-        ylabel(L"$\rho_{DCCA}(s)$")
-    end
+    return x,y
 end
+
 export rhoDCCA
+
 end
   
