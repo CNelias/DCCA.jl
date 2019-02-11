c62d6d93cb84d16972e7dbd1405e10387ff4c191
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2019-02-11 22:25:51 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using Test
 using DCCA
 
 x1 = rand(1000); x2 = rand(1000)
-print(rhoDCCA(x1,x2,20,200,30)
+print(rhoDCCA(x1,x2,20,200,30))
 @test rhoDCCA(x1,x2,20,200,30) == rhoDCCA(x1,x2,20,200,30)
