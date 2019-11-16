9aee2fe630934dc7ca76a54387e37d898fe31f5a
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2019-11-16 09:53:51 +0100

    Update DCCA.jl

--- src/DCCA.jl
+++ src/DCCA.jl
@@ -2,7 +2,7 @@ module DCCA
 
 include("calculs.jl")
 
-function rhoDCCA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int)
+function rhoDCCA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int; step = 10)
     x = log_space(box_b,box_s,nb_pt)
     y = dcca(data1,data2,box_b,box_s,nb_pt; fit_type = "polynomial")
     return x,y
