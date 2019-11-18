02d386ca2607f5f59753a95620bca3cd3f714156
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2019-11-18 16:34:11 +0100

    Update DCCA.jl

--- src/DCCA.jl
+++ src/DCCA.jl
@@ -2,13 +2,19 @@ module DCCA
 
 include("calculs.jl")
 
-function rhoDCCA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int; step = 10)
-    x = log_space(box_b,box_s,nb_pt)
-    y = dcca(data1,data2,box_b,box_s,nb_pt; fit_type = "polynomial")
+"""
+    rhoDCCA(x,y; box_start = 3, box_stop = div(length(x),10), nb_pts = 30)
+
+Performs the DCCA analysis of `x` and `y`. The default analysis starts with a window size of 3 up to one tenth of the total length of `x` for statistical reasons.
+
+returns the different window sizes used for the analysis, and the associated dcca coefficients.
+"""
+function rhoDCCA(x::Array{Float64,1},y::Array{Float64,1}; box_start = 3, box_stop = div(length(x),10), nb_pts = 30)
+    x = log_space(box_start,box_stop,nb_pts)
+    y = dcca(data1,data2; box_start = box_start, box_stop = box_stop, nb_pts = nb_pts)
     return x,y
 end
 
 export rhoDCCA
 
 end
-  
